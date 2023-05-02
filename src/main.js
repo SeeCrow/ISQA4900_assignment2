9f5c1e20054d597b499066be6d37f3c9f874abd9
Cameron Croghan <cameron.croghan@gmail.com>
2023-05-02 12:17:05 -0500

    full files.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+ import App from './App.vue'
+ import router from './router'
+ import store from './store'
+ import axios from 'axios'
+ import "bootstrap/dist/css/bootstrap.min.css"
+ import "bootstrap"
+ 
+createApp(App).use(store, axios).use(router).mount('#app')
